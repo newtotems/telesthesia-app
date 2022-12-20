00dc94da4e980386baa6cbf6a3c78759fa243c52
david-hogg <theboydavid+github@gmail.com>
2022-12-20 16:28:42 +0000

    Update lookupcards.js
    d

--- functions/lookupcards.js
+++ functions/lookupcards.js
@@ -6,30 +6,25 @@ exports.handler = async function(event, context) {
     secret: process.env.DB_SECRT
   });
 
-  // Retrieve the list of ID numbers from the query string parameters
-  const values = event.queryStringParameters.ids;
-  // Parse the values as an array of integers
-  const valuesArray = values.split(',').map(val => parseInt(val, 10));
-  // Create a new array called "firstFive" that contains the first 5 elements of valuesArray
-  const firstFive = valuesArray.slice(0, 5);
-
-
   try {
-    // Query FaunaDB to get the documents with the specified ID numbers in the 'cards' collection
+    // Get the 'values' query parameter from the event object
+    const values = event.queryStringParameters.values;
+
+    // Parse the values as an array of integers
+    const valuesArray = values.split(",").map(val => parseInt(val, 10));
+
+    // Create a new array called "firstFive" that contains the first 5 elements of valuesArray
+    const firstFive = valuesArray.slice(0, 5);
+
+    // Query FaunaDB to get all documents in the 'cards' collection with an ID in the 'firstFive' array
     const result = await client.query(
       faunadb.query.Map(
+        // Get all documents in the 'cards' collection where the 'id' value is in the 'firstFive' array
         faunadb.query.Filter(
-          valuesArray,
-          faunadb.query.Lambda(
-            "id",
-            faunadb.query.Get(
-              faunadb.query.Match(
-                faunadb.query.Index("cards_by_id"),
-                faunadb.query.Var("id")
-              )
-            )
-          )
+          faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index("all_cards"))),
+          faunadb.query.Lambda("X", faunadb.query.Contains(firstFive, faunadb.query.Select("id", faunadb.query.Var("X"))))
         ),
+        // Map over each document and return the data
         faunadb.query.Lambda("X", faunadb.query.Get(faunadb.query.Var("X")))
       )
     );
@@ -44,33 +39,33 @@ exports.handler = async function(event, context) {
     // Add up the 'individualvalue' values for all documents and store the result in 'allvalues'
     let allvalues = data.reduce((acc, curr) => acc + curr.individualvalue, 0);
 
-         // Iterate over the documents in the 'data' array
-         data.forEach(doc => {
-            if (doc.rules) {
-              // Iterate over the keys in the 'rules' object
-              Object.keys(doc.rules).forEach(key => {
-                const rule = doc.rules[key];
-    
-                // Check the 'type' of the rule
-                if (rule.type === "additive") {
-                  // Check if the 'criteria' of the rule is satisfied
-                  if (rule.criteria.type === doc.type) {
-                    // Adjust the 'allvalues' value based on the 'value' of the rule and the number of criteria instances
-                    allvalues += rule.value * Object.keys(doc.scores).length;
+    // Iterate over the documents in the 'data' array
+    data.forEach(doc => {
+      if (doc.rules) {
+        // Iterate over the keys in the 'rules' object
+        Object.keys(doc.rules).forEach(key => {
+          const rule = doc.rules[key];
+
+                   // Check the 'type' of the rule
+                   if (rule.type === "additive") {
+                    // Check if the 'criteria' of the rule is satisfied
+                    if (rule.criteria.type === doc.type) {
+                      // Adjust the 'allvalues' value based on the 'value' of the rule and the number of criteria instances
+                      allvalues += rule.value * Object.keys(doc.scores).length;
+                    }
+                  } else if (rule.type === "subtractive") {
+                    // Check if the 'criteria' of the rule is satisfied
+                    if (rule.criteria.type === doc.type) {
+                      // Adjust the 'allvalues' value based on the 'value' of the rule and the number of criteria instances
+                      allvalues -= rule.value * Object.keys(doc.scores).length;
+                    }
                   }
-                } else if (rule.type === "subtractive") {
-                  // Check if the 'criteria' of the rule is satisfied
-                  if (rule.criteria.type === doc.type) {
-                    // Adjust the 'allvalues' value based on the 'value' of the rule and the number of criteria instances
-                    allvalues -= rule.value * Object.keys(doc.scores).length;
-                  }
-                }
-              });
-            }
-          });
-    
-             // Return the 'individualvalue' array and the 'allvalues' score
-             return {
+                });
+              }
+            });
+        
+            // Return the 'individualvalue' array and the 'allvalues' score
+            return {
               statusCode: 200,
               body: JSON.stringify({
                 individualvalues: data.map(doc => doc.individualvalue),
@@ -88,4 +83,4 @@ exports.handler = async function(event, context) {
             };
           }
         };
-    
+        
