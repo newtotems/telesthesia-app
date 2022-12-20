b1f81ed88382cea9a4b39e722a922e8a0506935f
david-hogg <theboydavid+github@gmail.com>
2022-12-20 17:38:17 +0000

    Update lookupcards.js
    s

--- functions/lookupcards.js
+++ functions/lookupcards.js
@@ -23,7 +23,7 @@ exports.handler = async function(event, context) {
     // Query FaunaDB to get all documents in the 'cards' collection whose id values are in the 'valuesArray'
     const result = await client.query(
       faunadb.query.Map(
-        faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index("all_cards"), faunadb.query.In(faunadb.query.Var("values"), valuesArray))),
+        faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index("all_cards"), faunadb.query.In(faunadb.query.Var("id"), valuesArray))),
         faunadb.query.Lambda("X", faunadb.query.Get(faunadb.query.Var("X")))
       )
     );
@@ -38,48 +38,48 @@ exports.handler = async function(event, context) {
     // Add up the 'individualvalue' values for all documents and store the result in 'allvalues'
     let allvalues = data.reduce((acc, curr) => acc + curr.individualvalue, 0);
 
-       // Iterate over the documents in the 'data' array
-       data.forEach(doc => {
-        if (doc.rules) {
-          // Iterate over the keys in the 'rules' object
-          Object.keys(doc.rules).forEach(key => {
-            const rule = doc.rules[key];
-  
-            // Check the 'type' of the rule
-            if (rule.type === "additive") {
-              // Check if the 'criteria' of the rule is satisfied
-              if (rule.criteria.type === doc.type) {
-                // Adjust the 'allvalues' value based on the 'value' of the rule and the number of criteria instances
-                allvalues += rule.value * Object.keys(doc.scores).length;
-              }
-            } else if (rule.type === "subtractive") {
-              // Check if the 'criteria' of the rule is satisfied
-              if (rule.criteria.type === doc.type) {
-                // Adjust the 'allvalues' value based on the 'value' of the rule and the number of criteria instances
-                allvalues -= rule.value * Object.keys(doc.scores).length;
-              }
+        // Iterate over the documents in the 'data' array
+        data.forEach(doc => {
+            if (doc.rules) {
+              // Iterate over the keys in the 'rules' object
+              Object.keys(doc.rules).forEach(key => {
+                const rule = doc.rules[key];
+      
+                // Check the 'type' of the rule
+                if (rule.type === "additive") {
+                  // Check if the 'criteria' of the rule is satisfied
+                  if (rule.criteria.type === doc.type) {
+                    // Adjust the 'allvalues' value based on the 'value' of the rule and the number of criteria instances
+                    allvalues += rule.value * Object.keys(doc.scores).length;
+                  }
+                } else if (rule.type === "subtractive") {
+                  // Check if the 'criteria' of the rule is satisfied
+                  if (rule.criteria.type === doc.type) {
+                    // Adjust the 'allvalues' value based on the 'value' of the rule and the number of criteria instances
+                    allvalues -= rule.value * Object.keys(doc.scores).length;
+                  }
+                }
+              });
             }
           });
-        }
-      });
-  
-         // Return the 'individualvalue' array and the 'allvalues' score
-         return {
-          statusCode: 200,
-          body: JSON.stringify({
-            individualvalues: data.map(doc => doc.individualvalue),
-            allvalues
-          })
+      
+             // Return the 'individualvalue' array and the 'allvalues' score
+             return {
+              statusCode: 200,
+              body: JSON.stringify({
+                individualvalues: data.map(doc => doc.individualvalue),
+                allvalues
+              })
+            };
+          } catch (error) {
+            // Log the error message
+            console.error(error);
+        
+            // Return a server error response
+            return {
+              statusCode: 500,
+              body: JSON.stringify({ error: "Error getting data from FaunaDB" })
+            };
+          }
         };
-      } catch (error) {
-        // Log the error message
-        console.error(error);
-    
-        // Return a server error response
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ error: "Error getting data from FaunaDB" })
-        };
-      }
-    };
-  
+      
