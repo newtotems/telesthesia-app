a4eb7d324523cbe824606a17237cac76bec16800
david-hogg <theboydavid+github@gmail.com>
2022-12-20 15:56:57 +0000

    Update lookupcards.js
    id

--- functions/lookupcards.js
+++ functions/lookupcards.js
@@ -2,21 +2,21 @@ const faunadb = require("faunadb");
 const q = faunadb.query;
 
 exports.handler = async (event, context) => {
-  // Get the FaunaDB secret and the query parameter called "names"
+  // Get the FaunaDB secret and the query parameter called "ids"
   const secret = process.env.DB_SECRT;
-  const names = event.queryStringParameters.names;
+  const ids = event.queryStringParameters.ids;
 
-  // Parse the names as an array
-  const namesArray = names.split(',');
+  // Parse the ids as an array
+  const idsArray = ids.split(',');
 
   // Connect to FaunaDB using the secret
   const client = new faunadb.Client({ secret });
 
   try {
-    // Query the cards collection to find all records with a name that is in the namesArray
+    // Query the cards collection to find all records with an ID that is in the idsArray
     const response = await client.query(
       q.Map(
-        q.Paginate(q.Match(q.Index("cards_by_name"), namesArray)),
+        q.Paginate(q.Match(q.Index("cards_by_id"), idsArray)),
         q.Lambda("X", q.Get(q.Var("X")))
       )
     );
