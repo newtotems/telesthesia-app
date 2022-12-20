a731229dc2a31b65e7622ef44591e6c8cf1c3a48
david-hogg <theboydavid+github@gmail.com>
2022-12-20 17:58:51 +0000

    Update lookupcards.js
    d

--- functions/lookupcards.js
+++ functions/lookupcards.js
@@ -23,12 +23,14 @@ exports.handler = async function(event, context) {
     // Query FaunaDB to get all documents in the 'cards' collection whose id values are in the 'valuesArray'
     // Query FaunaDB to get all documents in the 'cards' collection whose id values are contained in the 'valuesArray'
 // Query FaunaDB to get all documents in the 'cards' collection whose id values are contained in the 'valuesArray'
+// Query FaunaDB to get all documents in the 'cards' collection whose id values are contained in the 'valuesArray'
 const result = await client.query(
     faunadb.query.Map(
-      faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index("all_cards"), faunadb.query.Contains(faunadb.query.Var("id"), valuesArray))),
+      faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index("all_cards"), faunadb.query.Contains(id, valuesArray))),
       faunadb.query.Lambda("X", faunadb.query.Get(faunadb.query.Var("X")))
     )
   );
+  
     
 
     // Extract the data from the result
