b28e07098871d6db63f286d136bd507ff6bd8942
david-hogg <theboydavid+github@gmail.com>
2022-12-20 15:06:09 +0000

    Update gettotals.js
    d

--- functions/gettotals.js
+++ functions/gettotals.js
@@ -17,39 +17,16 @@ exports.handler = async function(event, context) {
 
     // Extract the data from the result
     const data = result.data.map(doc => {
-      // Add up the values in the 'scores' object and store the total in a new property called 'totalscores'
-      let totalscores = Object.values(doc.data.scores).reduce((acc, curr) => acc + curr, 0);
-
-      // Check if the document has a 'rules' object
-      if (doc.data.rules) {
-        // Iterate over the keys in the 'rules' object
-        Object.keys(doc.data.rules).forEach(key => {
-          const rule = doc.data.rules[key];
-
-          // Check the 'type' of the rule
-          if (rule.type === "additive") {
-            // Check if the 'criteria' of the rule is satisfied
-            if (rule.criteria.type === doc.data.type) {
-              // Adjust the 'totalscores' value based on the 'value' of the rule
-              totalscores += rule.value;
-            }
-          } else if (rule.type === "subtractive") {
-            // Check if the 'criteria' of the rule is satisfied
-            if (rule.criteria.type === doc.data.type) {
-              // Adjust the 'totalscores' value based on the 'value' of the rule
-              totalscores -= rule.value;
-            }
-          }
-        });
-      }
-
-      return { ...doc.data, totalscores };
+      // Add up the values in the 'scores' object and store the total in a new property called 'individualvalue'
+      let individualvalue = Object.values(doc.data.scores).reduce((acc, curr) => acc + curr, 0);
+      return { ...doc.data, individualvalue };
     });
 
-    // Add up the 'totalscores' values for all documents and store the result in 'overallscore'
-    let overallscore = 0;
+    // Add up the 'individualvalue' values for all documents and store the result in 'allvalues'
+    let allvalues = data.reduce((acc, curr) => acc + curr.individualvalue, 0);
+
+    // Iterate over the keys in the 'rules' object
     data.forEach(doc => {
-      // Iterate over the keys in the 'rules' object
       if (doc.rules) {
         Object.keys(doc.rules).forEach(key => {
           const rule = doc.rules[key];
@@ -58,24 +35,24 @@ exports.handler = async function(event, context) {
           if (rule.type === "additive") {
             // Check if the 'criteria' of the rule is satisfied
             if (rule.criteria.type === doc.type) {
-              // Adjust the 'overallscore' value based on the 'value' of the rule
-              overallscore += rule.value;
+              // Adjust the 'allvalues' value based on the 'value' of the rule
+              allvalues += rule.value;
             }
           } else if (rule.type === "subtractive") {
             // Check if the 'criteria' of the rule is satisfied
             if (rule.criteria.type === doc.type) {
-              // Adjust the 'overallscore' value based on the 'value' of the rule
-              overallscore -= rule.value;
+              // Adjust the 'allvalues' value based on the 'value' of the rule
+              allvalues -= rule.value;
             }
           }
         });
       }
     });
 
-    // Return the 'overallscore'
+    // Return the 'allvalues'
     return {
       statusCode: 200,
-      body: JSON.stringify({ overallscore })
+      body: JSON.stringify({ allvalues })
     };
   } catch (error) {
     // Log the error message
