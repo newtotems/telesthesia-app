46ba60502dd3ed5401feb6bd50095bcc7d8b3d34
david-hogg <71979980+newtotems@users.noreply.github.com>
2022-12-24 15:01:06 +0000

    ddd
    dd

--- functions/checklocation.js
+++ functions/checklocation.js
@@ -1,62 +1,65 @@
-const faunadb = require('faunadb');
+const faunadb = require('faunadb')
+
+// Create a new FaunaDB client
+const client = new faunadb.Client({
+  secret: process.env.DB_SECRT
+})
 
 exports.handler = async (event, context) => {
-  // Parse the lat and long values from the request body
-  const body = JSON.parse(event.body);
-  const lat = Number(body.lat);
-  const lng = Number(body.lng);
+  // Parse the request body
+  const body = JSON.parse(event.body)
+  const lat = Number(body.lat)
+  const latstring = lat.toString()
+  const lng = Number(body.lng)
+  const lngstring = lng.toString()
 
-  console.log(lat,lng);
+  const latlng = latstring + lngstring
+  console.log(latlng)
+  console.log(typeof latlng)
 
-  // Create a FaunaDB client
-  const client = new faunadb.Client({ secret: process.env.DB_SECRT });
-
-  // Try to find a matching record in the locations_by_latlong index
+  // Try to retrieve a matching location from the 'all_locations' index
   try {
+    // Retrieve the matching location from the 'all_locations' index
     const result = await client.query(
       faunadb.query.Get(
-        faunadb.query.Match(faunadb.query.Index('locations_by_latlong'), [lat, lng])
+        faunadb.query.Match(
+          faunadb.query.Index('locations_latlong'),latlng
+        )
       )
-    );
+    )
 
-    // Return the image and text fields from the matching record
+    // Return the 'image' and 'text' fields from the matching location in the response
     return {
       statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
       body: JSON.stringify({
         image: result.data.image,
         text: result.data.text
       })
-    };
-  } catch (error) {
-    // Fetch the record with the ID of 1 from the all_negative_responses index
-    try {
-      const record = await client.query(
-        faunadb.query.Get(faunadb.query.Ref(faunadb.query.Match(faunadb.query.Index('all_negative_responses_by_id'), 1)))
-      );
-
-
-      // Return the text field from the fetched record
-      return {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({
-          text: record.data.text
-        })
-      };
-    } catch (error) {
-      // Return a message if an error occurred
-      return {
-        statusCode: 500,
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        },
-        body: 'ERROR unknwn'
-      };
     }
+  } catch (error) {
+     // If there is no matching location, read all records in the 'all_neg_responses' collection
+  const allRecords = await client.query(
+    faunadb.query.Map(
+      faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index('all_neg_responses'))),
+      faunadb.query.Lambda(
+        'record',
+        faunadb.query.Var('record')
+      )
+    )
+  )
+
+  // Get the number of records in the 'all_neg_responses' collection
+  const numRecords = allRecords.data.length
+
+  // Choose a random index between 0 and numRecords - 1
+  const randomIndex = Math.floor(Math.random() * numRecords)
+
+  // Return the record at the random index in the 'all_neg_responses' collection in the response
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      text: allRecords.data[randomIndex]
+    })
   }
-};
+  }
+}
