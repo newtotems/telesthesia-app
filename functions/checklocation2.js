2f6d239317e10b24e05cfe03e8143e41c6873fb9
david-hogg <71979980+newtotems@users.noreply.github.com>
2022-12-24 13:12:05 +0000

    Update checklocation2.js
    dd

--- functions/checklocation2.js
+++ functions/checklocation2.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
   const result = await client.query(
     faunadb.query.Map(
       // Get all documents in the 'locations' collection
-      faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index('all_locations'))),
+      faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index('locations_by_latlong'))),
       // Retrieve the document data for each location
       faunadb.query.Lambda((ref) => faunadb.query.Get(ref))
     )
