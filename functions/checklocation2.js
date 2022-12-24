96571b80e8938502a5e34c107493944883ada03f
david-hogg <71979980+newtotems@users.noreply.github.com>
2022-12-24 11:01:49 +0000

    Update checklocation2.js
    d

--- functions/checklocation2.js
+++ functions/checklocation2.js
@@ -6,25 +6,16 @@ const client = new faunadb.Client({
 })
 
 exports.handler = async (event, context) => {
-  // Query the 'all_negative_responses_by_id' index to retrieve all negative responses
+  // Query the 'all_negative_responses' collection to get a random negative response
   const result = await client.query(
-    faunadb.query.Map(
-      // Get all documents from the 'all_negative_responses_by_id' index
-      faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index('all_negative_responses_by_id'))),
-      // Retrieve the document data for each negative response
-      faunadb.query.Lambda((ref) => faunadb.query.Get(ref))
+    faunadb.query.Get(
+      faunadb.query.Random(faunadb.query.Match(faunadb.query.Index('all_negative_responses')))
     )
   )
-
-  // Select a random negative response from the list of all negative responses
-  const randomIndex = Math.floor(Math.random() * result.data.length)
-  const negativeResponse = result.data[randomIndex]
-  const nr = JSON.stringify(negativeResponse)
-  console.log(nr)
 
   // Return the selected negative response
   return {
     statusCode: 200,
-    body: JSON.stringify(negativeResponse)
+    body: JSON.stringify(result.data)
   }
 }
