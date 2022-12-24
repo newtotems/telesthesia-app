b9a829cf001893f5f93a59d9343d3b6b5cfde42e
david-hogg <71979980+newtotems@users.noreply.github.com>
2022-12-24 12:50:53 +0000

    Update checklocation2.js
    d

--- functions/checklocation2.js
+++ functions/checklocation2.js
@@ -6,37 +6,20 @@ const client = new faunadb.Client({
 })
 
 exports.handler = async (event, context) => {
-  // Parse the request body to get the 'lat' and 'long' parameters
-  const body = JSON.parse(event.body)
-  const lat = body.lat
-  const long = body.lng
-
-  // Query the 'locations_by_latlong' index to retrieve all matching records
+  // Check if the 'locations_by_latlong' index exists
   const result = await client.query(
-    faunadb.query.Map(
-      // Get all documents from the 'locations_by_latlong' index that match the 'lat' and 'long' values
-      faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index('locations_by_latlong'), [lat, long])),
-      // Retrieve the document data for each matching record
-      faunadb.query.Lambda((ref) => faunadb.query.Get(ref))
-    )
+    faunadb.query.Exists(faunadb.query.Index('locations_by_latlong'))
   )
 
-  // Check if any matching records were found
-  if (result.data.length > 0) {
-    // Select a random matching record from the list
-    const randomIndex = Math.floor(Math.random() * result.data.length)
-    const matchingRecord = result.data[randomIndex]
-
-    // Return the 'image' and 'text' fields from the random matching record
+  // Check if the index exists
+  if (result.data) {
+    // Return 'true' if the index exists
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        image: matchingRecord.image,
-        text: matchingRecord.text
-      })
+      body: JSON.stringify(true)
     }
   } else {
-    // If there are no matching records, get a random negative response from the 'all_negative_responses' collection
+    // If the index does not exist, get a random negative response from the 'all_negative_responses' collection
     const negativeResult = await client.query(
       faunadb.query.Get(
         faunadb.query.Random(faunadb.query.Match(faunadb.query.Index('all_negative_responses')))
