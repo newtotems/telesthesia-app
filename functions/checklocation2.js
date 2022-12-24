6a0c95af27197e77c63406a29c17308f2fc28295
david-hogg <71979980+newtotems@users.noreply.github.com>
2022-12-24 13:07:15 +0000

    Update checklocation2.js
    d

--- functions/checklocation2.js
+++ functions/checklocation2.js
@@ -6,41 +6,22 @@ const client = new faunadb.Client({
 })
 
 exports.handler = async (event, context) => {
-  // Check if the 'locations_by_latlong' index exists
+  // Get all collections in the database
   const result = await client.query(
-    faunadb.query.Exists(faunadb.query.Collection('locations'))
+    faunadb.query.Map(
+      // Get all collections
+      faunadb.query.Paginate(faunadb.query.Collections()),
+      // Retrieve the name of each collection
+      faunadb.query.Lambda((ref) => faunadb.query.Get(ref))
+    )
   )
 
-  // Check if the index exists
-  if (result.data) {
-    console.log('Locations collection exists')
-    // Return 'true' if the index exists
-    return {
-      statusCode: 200,
-      body: JSON.stringify(true)
-    }
-  } else {
-    console.log('Locations collection does not exist')
-    // If the index does not exist, get all negative responses from the 'all_negative_responses' collection
-    const negativeResult = await client.query(
-      faunadb.query.Map(
-        // Get all documents from the 'all_negative_responses' collection
-        faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index('all_negative_responses'))),
-        // Retrieve the document data for each negative response
-        faunadb.query.Lambda((ref) => faunadb.query.Get(ref))
-      )
-    )
-
-    // Select a random negative response from the list
-    const randomIndex = Math.floor(Math.random() * negativeResult.data.length)
-    const negativeResponse = negativeResult.data[randomIndex]
-
-    // Return the 'text' field from the random negative response
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        text: negativeResponse.text
-      })
-    }
+  // Return the names of the collections in the response
+  const collectionNames = result.data.map((collection) => collection.name)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      collections: collectionNames
+    })
   }
 }
