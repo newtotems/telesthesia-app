8dc1355f7cea15f57f0f1ca1f920092ba9117af7
david-hogg <71979980+newtotems@users.noreply.github.com>
2022-12-24 13:59:25 +0000

    Update checklocation2.js
    dddd

--- functions/checklocation2.js
+++ functions/checklocation2.js
@@ -9,23 +9,23 @@ exports.handler = async (event, context) => {
   // Parse the request body
   const body = JSON.parse(event.body)
   const lat = Number(body.lat)
-  const latstring = lat.toString()
   const lng = Number(body.lng)
-  const lngstring = lng.toString()
-
-  const latlng = latstring + lngstring
-  console.log(latlng)
-  console.log(typeof latlng)
 
   // Try to retrieve a matching location from the 'all_locations' index
   try {
     // Retrieve the matching location from the 'all_locations' index
     const result = await client.query(
-      faunadb.query.Get(
-        faunadb.query.Match(
-          faunadb.query.Index('all_locations'),
-          faunadb.query.Equals(faunadb.query.Var('latlng'), latlng)
-        )
+      faunadb.query.Map(
+        faunadb.query.Paginate(
+          faunadb.query.Match(
+            faunadb.query.Index('all_locations'),
+            faunadb.query.And(
+              faunadb.query.Equals(faunadb.query.Var('lat'), lat),
+              faunadb.query.Equals(faunadb.query.Var('lng'), lng)
+            )
+          )
+        ),
+        faunadb.lambda((x) => faunadb.query.Get(x))
       )
     )
 
@@ -33,8 +33,8 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        image: result.data.image,
-        text: result.data.text
+        image: result.data[0].data.image,
+        text: result.data[0].data.text
       })
     }
   } catch (error) {
