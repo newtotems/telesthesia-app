7c18fed54bb89e71d446c2f16597144cac71f745
newtotems <71979980+newtotems@users.noreply.github.com>
2023-04-10 11:02:32 +0100

    updated checklocations & 3 dec places
    updated checklocations to check exact match for both lat and lon and also changed to send 3 decimal place lat lons

--- functions/checklocation.js
+++ functions/checklocation.js
@@ -1,32 +1,27 @@
-const faunadb = require('faunadb')
+const faunadb = require('faunadb');
 
 // Create a new FaunaDB client
 const client = new faunadb.Client({
   secret: process.env.DB_SECRT
-})
+});
 
 exports.handler = async (event, context) => {
   // Parse the request body
-  const body = JSON.parse(event.body)
-  const lat = Number(body.lat)
-  const latstring = lat.toString()
-  const lng = Number(body.lng)
-  const lngstring = lng.toString()
+  const body = JSON.parse(event.body);
+  const lat = Number(body.lat);
+  const lng = Number(body.lng);
 
-  const latlng = latstring + lngstring
-  console.log(latlng)
-  console.log(typeof latlng)
-
-  // Try to retrieve a matching location from the 'all_locations' index
+  // Try to retrieve a matching location from the 'locations_lat_and_lon' index
   try {
-    // Retrieve the matching location from the 'all_locations' index
+    // Retrieve the matching location from the 'locations_lat_and_lon' index
     const result = await client.query(
       faunadb.query.Get(
         faunadb.query.Match(
-          faunadb.query.Index('locations_latlong'),latlng
+          faunadb.query.Index('locations_lat_and_lon'),
+          [lat, lng]
         )
       )
-    )
+    );
 
     // Return the 'image' and 'text' fields from the matching location in the response
     return {
@@ -40,32 +35,32 @@ exports.handler = async (event, context) => {
       })
     }
   } catch (error) {
-     // If there is no matching location, read all records in the 'all_neg_responses' collection
-  const allRecords = await client.query(
-    faunadb.query.Map(
-      faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index('all_neg_responses'))),
-      faunadb.query.Lambda(
-        'record',
-        faunadb.query.Var('record')
+    // If there is no matching location, read all records in the 'all_neg_responses' collection
+    const allRecords = await client.query(
+      faunadb.query.Map(
+        faunadb.query.Paginate(faunadb.query.Match(faunadb.query.Index('all_neg_responses'))),
+        faunadb.query.Lambda(
+          'record',
+          faunadb.query.Var('record')
+        )
       )
-    )
-  )
+    );
 
-  // Get the number of records in the 'all_neg_responses' collection
-  const numRecords = allRecords.data.length
+    // Get the number of records in the 'all_neg_responses' collection
+    const numRecords = allRecords.data.length;
 
-  // Choose a random index between 0 and numRecords - 1
-  const randomIndex = Math.floor(Math.random() * numRecords)
+    // Choose a random index between 0 and numRecords - 1
+    const randomIndex = Math.floor(Math.random() * numRecords);
 
-  // Return the record at the random index in the 'all_neg_responses' collection in the response
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      lat: lat,
-      lng: lng,
-      text: allRecords.data[randomIndex],
-      success: false
-    })
+    // Return the record at the random index in the 'all_neg_responses' collection in the response
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        lat: lat,
+        lng: lng,
+        text: allRecords.data[randomIndex],
+        success: false
+      })
+    }
   }
-  }
-}
+};
