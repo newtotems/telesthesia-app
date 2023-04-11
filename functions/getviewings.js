ad62cfba9645807957aed8cd3ded7f6b5385ae77
newtotems <71979980+newtotems@users.noreply.github.com>
2023-04-11 14:15:08 +0100

    Update getviewings.js

--- functions/getviewings.js
+++ functions/getviewings.js
@@ -1,33 +1,34 @@
 const faunadb = require("faunadb");
 const q = faunadb.query;
+const client = new faunadb.Client({ secret: process.env.DB_SECRT });
 
 exports.handler = async (event, context) => {
   try {
-    const client = new faunadb.Client({
-      secret: process.env.DB_SECRT,
-    });
-    
     const response = await client.query(
       q.Map(
         q.Paginate(q.Match(q.Index("all_locations"))),
         q.Lambda(
           "locationRef",
-          q.Get(q.Var("locationRef"))
+          q.Let(
+            {
+              locationDoc: q.Get(q.Var("locationRef")),
+              viewing: q.Select(["data", "viewing"], q.Var("locationDoc"), null)
+            },
+            q.If(q.IsNull(q.Var("viewing")), null, q.Var("viewing"))
+          )
         )
       )
     );
 
-    const viewings = response.data.map((location) => ({
-      viewing: location.data.viewing,
-    }));
-
-    const count = response.data.length;
-
     return {
       statusCode: 200,
-      body: JSON.stringify({ viewings, count }),
+      body: JSON.stringify(response)
     };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    };
   }
 };
