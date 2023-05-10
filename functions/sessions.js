5e1ee19dc0122811a00f0e2a4f0487207789fb42
newtotems <71979980+newtotems@users.noreply.github.com>
2023-05-10 16:20:00 +0100

    ip

--- functions/sessions.js
+++ functions/sessions.js
@@ -5,56 +5,22 @@ const client = new faunadb.Client({
   secret: process.env.DB_SECRT,
 });
 
-// Function to generate a random session ID
-function generateSessionId() {
-  // Generate a random string using alphanumeric characters
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const length = 16;
-  let sessionId = '';
-  for (let i = 0; i < length; i++) {
-    sessionId += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return sessionId;
-}
-
-// Function to create a new session in FaunaDB
-async function createSession() {
-  const sessionId = generateSessionId();
-  await client.query(
-    q.Create(q.Collection('sessions'), {
-      data: {
-        sessionId: sessionId,
-        requestCount: 0,
-        timestamp: new Date().getTime(),
-      },
-    })
-  );
-  return sessionId;
-}
-
-exports.handler = async (event, context) => {
-  // Retrieve the session ID from the request headers
-  const sessionId = event.headers['X-Session-ID'];
-
+// Function to check if an IP address has exceeded the rate limit
+async function isRateLimitExceeded(ipAddress) {
   try {
-    // Retrieve the session data from FaunaDB based on the session ID
     const { data } = await client.query(
-      q.Get(q.Match(q.Index('sessions_by_sessionId'), sessionId))
+      q.Get(q.Match(q.Index('rate_limit_by_ipAddress'), ipAddress))
     );
-
-    // Get current timestamp
     const currentTimestamp = new Date().getTime();
-
+    const timeDifference = currentTimestamp - data.timestamp;
+    
     // Check if rate limit exceeded
-    if (currentTimestamp - data.timestamp < 60000 && data.requestCount >= 2) {
-      return {
-        statusCode: 429,
-        body: JSON.stringify({ message: 'Rate limit exceeded.' }),
-      };
+    if (timeDifference < 60000 && data.requestCount >= 2) {
+      return true;
     }
 
     // Reset request count if one minute has passed
-    if (currentTimestamp - data.timestamp >= 60000) {
+    if (timeDifference >= 60000) {
       await client.query(
         q.Update(data.ref, {
           data: {
@@ -65,32 +31,65 @@ exports.handler = async (event, context) => {
       );
     }
 
+    return false;
+  } catch (error) {
+    if (error.name === 'NotFound') {
+      // Create a new rate limit entry for the IP address
+      await client.query(
+        q.Create(q.Collection('rate_limits'), {
+          data: {
+            ipAddress: ipAddress,
+            requestCount: 0,
+            timestamp: new Date().getTime(),
+          },
+        })
+      );
+      return false;
+    }
+
+    // Handle other errors
+    throw error;
+  }
+}
+
+// Function to increment the request count for an IP address
+async function incrementRequestCount(ipAddress) {
+  await client.query(
+    q.Update(q.Select('ref', q.Get(q.Match(q.Index('rate_limit_by_ipAddress'), ipAddress))), {
+      data: {
+        requestCount: q.Add(
+          q.Select(['data', 'requestCount'], q.Get(q.Match(q.Index('rate_limit_by_ipAddress'), ipAddress))),
+          1
+        ),
+        timestamp: new Date().getTime(),
+      },
+    })
+  );
+}
+
+exports.handler = async (event, context) => {
+  // Retrieve the IP address from the request headers
+  const ipAddress = event.headers['X-Forwarded-For'] || event.headers['X-Real-IP'];
+
+  try {
+    // Check if rate limit exceeded
+    if (await isRateLimitExceeded(ipAddress)) {
+      return {
+        statusCode: 429,
+        body: JSON.stringify({ message: 'Rate limit exceeded.' }),
+      };
+    }
+
     // Increment request count
-    await client.query(
-      q.Update(data.ref, {
-        data: {
-          requestCount: data.requestCount + 1,
-          timestamp: currentTimestamp,
-        },
-      })
-    );
+    await incrementRequestCount(ipAddress);
 
     // Proceed with the request
     return {
       statusCode: 200,
-      body: JSON.stringify({ sessionId: sessionId, message: 'Session exists.' }),
+      body: JSON.stringify({ message: 'Request processed.' }),
     };
   } catch (error) {
-    if (error.name === 'NotFound') {
-      // Create a new session if it doesn't exist
-      const newSessionId = await createSession();
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ sessionId: newSessionId, message: 'New session created.' }),
-      };
-    }
-
-    // Handle other errors
+    // Handle errors
     return {
       statusCode: 500,
       body: JSON.stringify({ message: 'Internal server error.' }),
